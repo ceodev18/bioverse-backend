cead2653b8cc742ad2c9a198a1b20ab6a7db9e78
Italo Espinoza <cochoa@rccl.com>
2025-02-14 14:23:17 -0500

    let cors works

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
-  app.enableCors({
-    origin: "http://localhost:3000", // ✅ Allow requests from Next.js
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  });
+  app.enableCors();
   app.setGlobalPrefix('api')
   const PORT = process.env.PORT || 4000;
   app.use(express.json());
